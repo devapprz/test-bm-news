7bdecee300acecefe0281195bc6c24af88e3ba3b
yusufrizkyana <devapprz@gmail.com>
2022-10-14 18:51:12 +0700

    fix search method  #13

--- app/src/main/java/com/yusuf/bankmandiri/newsapps/feature/sources/SourceRepository.kt
+++ app/src/main/java/com/yusuf/bankmandiri/newsapps/feature/sources/SourceRepository.kt
@@ -8,31 +8,28 @@ import javax.inject.Inject
 
 @ViewModelScoped
 class SourceRepository
-@Inject
-constructor(
+@Inject constructor(
     private val sourceRemote: SourceRemote
 ) {
 
     private val _sourceCache = mutableListOf<Source>()
 
-    fun findAll(category: String?) =
-        sourceRemote.findAll(category).onEach {
-            if (!it.message.isNullOrEmpty()) {
-                _sourceCache.clear()
-                _sourceCache.addAll(it.result.orEmpty())
-            }
+    fun findAll(category: String?) = sourceRemote.findAll(category).onEach {
+        if (it.message.isNullOrEmpty()) {
+            _sourceCache.clear()
+            _sourceCache.addAll(it.result.orEmpty())
         }
+    }
 
     fun find(search: String?) = flow {
         kotlinx.coroutines.delay(1500)
-        val result = if (search.isNullOrEmpty()) {
-            _sourceCache
-        } else {
-            _sourceCache.filter {
-                it.name?.contains(search) == true
+        emit(_sourceCache.filter {
+            if (search.isNullOrEmpty()) {
+                true
+            } else {
+                it.name?.lowercase()?.contains(search.lowercase()) == true
             }
-        }
-        emit(result)
+        })
     }
 
 }
