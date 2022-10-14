55bc4bc771317bb49e57bbaaf17cbdd7647300b4
yusufrizkyana <devapprz@gmail.com>
2022-10-14 08:14:03 +0700

    add remote layer for request #3

---
+++ app/src/main/java/com/yusuf/bankmandiri/newsapps/feature/sources/SourceRemote.kt
@@ -0,0 +1,23 @@
+package com.yusuf.bankmandiri.newsapps.feature.sources
+
+import android.content.Context
+import com.github.kittinunf.fuel.httpGet
+import com.yusuf.bankmandiri.newsapps.feature.sources.data.Source
+import com.yusuf.bankmandiri.newsapps.utils.readResult
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ViewModelScoped
+import javax.inject.Inject
+
+@ViewModelScoped
+class SourceRemote
+@Inject
+constructor(
+    @ApplicationContext
+    private val context: Context
+) {
+
+    fun findAll(category: String?) = "v2/top-headlines/sources"
+        .httpGet(listOf("category" to (category?.lowercase().orEmpty().ifEmpty { "technology" })))
+        .readResult<List<Source>>(context = context, key = "sources")
+
+}
