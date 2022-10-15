34552aacadeec75862bf0aab2f7c5b3c39f2053f
yusufrizkyana <devapprz@gmail.com>
2022-10-15 20:00:50 +0700

    update view model for support paging #17

--- app/src/main/java/com/yusuf/bankmandiri/newsapps/feature/sources/SourceViewModel.kt
+++ app/src/main/java/com/yusuf/bankmandiri/newsapps/feature/sources/SourceViewModel.kt
@@ -1,12 +1,13 @@
 package com.yusuf.bankmandiri.newsapps.feature.sources
 
-import android.content.Intent
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.cachedIn
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -20,30 +21,10 @@ constructor(
 
     val state = _state.asStateFlow()
 
-    fun findAll(category: String?) = viewModelScope.launch(Dispatchers.IO) {
-        sourceRepository.findAll(category)
-            .onStart {
-                _state.update { SourceState(isLoading = true) }
-            }
-            .catch { error ->
-                _state.update { SourceState(messages = error.message) }
-            }
-            .collectLatest { collect ->
-                _state.update { SourceState(sources = collect) }
-            }
-    }
-
-    fun find(search: String?) = viewModelScope.launch(Dispatchers.IO) {
-        sourceRepository.find(search = search)
-            .onStart {
-                _state.update { SourceState(isLoading = true) }
-            }
-            .catch { error ->
-                _state.update { SourceState(messages = error.message) }
-            }
-            .collectLatest { collect ->
-                _state.update { SourceState(sources = collect) }
-            }
-    }
+    fun findAll(category: String?, search: String?) =
+        Pager(config = PagingConfig(pageSize = 4))
+        { sourceRepository.findAll(category, search = search, pageSize = 4) }
+            .flow
+            .cachedIn(viewModelScope)
 
 }
